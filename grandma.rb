e7ef14c4bb5b5274ebd4b4add16b1587267171fe
lieby weintraub <liebahinda@gmail.com>
2019-02-17 21:36:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMKIN!"
-    elsif phrase.upcase
+    elsif !phrase.upcase
     "NO, NOT SINCE 1938!"
   else
   return  "HUH?! SPEAK UP, SONNY!"
